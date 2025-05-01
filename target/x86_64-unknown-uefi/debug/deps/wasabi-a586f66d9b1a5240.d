beb0a7823862b1866dc1996f5c1c8c7e8a334166
arkb <araki.knsk@gmail.com>
2025-05-01 21:22:14 +0900

    Implement screen clearing with solid green fill

---
+++ target/x86_64-unknown-uefi/debug/deps/wasabi-a586f66d9b1a5240.d
@@ -0,0 +1,9 @@
+/home/karaki/work/wasabi-osbook/repo/wasabi/target/x86_64-unknown-uefi/debug/deps/libwasabi-a586f66d9b1a5240.rmeta: src/main.rs Cargo.toml
+
+/home/karaki/work/wasabi-osbook/repo/wasabi/target/x86_64-unknown-uefi/debug/deps/wasabi-a586f66d9b1a5240.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
