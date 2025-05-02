38ce435e842880e4cc874f13142704a62e5e0164
arkb <araki.knsk@gmail.com>
2025-05-02 18:27:59 +0900

    Display the string 'ABCDEF' diagonally on the wasabi OS screen

--- target/x86_64-unknown-uefi/debug/deps/wasabi-ac554089f575eee1.d
+++ target/x86_64-unknown-uefi/debug/deps/wasabi-ac554089f575eee1.d
@@ -1,5 +1,6 @@
-/home/karaki/work/wasabi-osbook/repo/wasabi/target/x86_64-unknown-uefi/debug/deps/wasabi-ac554089f575eee1.efi: src/main.rs
+/home/karaki/work/wasabi-osbook/repo/wasabi/target/x86_64-unknown-uefi/debug/deps/wasabi-ac554089f575eee1.efi: src/main.rs src/./font.txt
 
-/home/karaki/work/wasabi-osbook/repo/wasabi/target/x86_64-unknown-uefi/debug/deps/wasabi-ac554089f575eee1.d: src/main.rs
+/home/karaki/work/wasabi-osbook/repo/wasabi/target/x86_64-unknown-uefi/debug/deps/wasabi-ac554089f575eee1.d: src/main.rs src/./font.txt
 
 src/main.rs:
+src/./font.txt:
--- target/x86_64-unknown-uefi/debug/wasabi.d
+++ target/x86_64-unknown-uefi/debug/wasabi.d
@@ -1,1 +1,1 @@
-/home/karaki/work/wasabi-osbook/repo/wasabi/target/x86_64-unknown-uefi/debug/wasabi.efi: /home/karaki/work/wasabi-osbook/repo/wasabi/src/main.rs
+/home/karaki/work/wasabi-osbook/repo/wasabi/target/x86_64-unknown-uefi/debug/wasabi.efi: /home/karaki/work/wasabi-osbook/repo/wasabi/src/font.txt /home/karaki/work/wasabi-osbook/repo/wasabi/src/main.rs
